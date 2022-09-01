9452eb47d7e4724205369f68303f14b03db64d62
Andres <andrescasa.dev@gmail.com>
2022-09-01 14:51:26 -0500

    design done

--- BlogeSpace/index.js
+++ BlogeSpace/index.js
@@ -4,15 +4,18 @@ const mount = document.querySelector('#mount');
 async function fetchPosts(){
   const response = await fetch(`${BaseURL}/posts`);
   const data = await response.json();
-  return data.slice(0, 5); 
+  return data.slice(0, 10); 
 }
 
 fetchPosts().then(posts => {
   const postsHTML = posts.reduce((html, post)=>{
     return html+`
     <div class='post'>
-      <h2>${post.title}</h2>
-      <p>${post.body}</p>
+      <div class='post__img'></div>
+      <div >
+        <h2>${post.title}</h2>
+        <p class='post__description'>${post.body}</p>
+      </div>
     </div>`
   },'');
   mount.innerHTML = postsHTML
