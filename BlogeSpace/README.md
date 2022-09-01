1ee6989bfefdc585938058bfa7c5ba104377dec2
Andres <andrescasa.dev@gmail.com>
2022-09-01 16:57:41 -0500

    form design

--- BlogeSpace/README.md
+++ BlogeSpace/README.md
@@ -1,2 +1,5 @@
 # UI design
 ![](https://cdn.dribbble.com/userupload/2623158/file/original-ff44a9f2238eef9a1e0b5e21d27e7f94.png?compress=1&resize=1200x900)
+
+# UI form design
+![]()
--- BlogeSpace/index.js
+++ BlogeSpace/index.js
@@ -1,17 +1,29 @@
 const BaseURL = 'https://apis.scrimba.com/jsonplaceholder';
 const mount = document.querySelector('#mount');
+const POST_NUMBER = 10;
 
 async function fetchPosts(){
   const response = await fetch(`${BaseURL}/posts`);
   const data = await response.json();
-  return data.slice(0, 10); 
+  return data.slice(0, POST_NUMBER); 
 }
 
-fetchPosts().then(posts => {
+/**
+ * @returns [{image},{image}]
+ */
+async function fetchImages(){
+  const response = await fetch(`https://picsum.photos/v2/list?limit=${POST_NUMBER}`)
+  return data = await response.json();
+}
+
+async function renderPost(){
+  const posts = await fetchPosts();
+  const images = await fetchImages();
+  let i = 0;
   const postsHTML = posts.reduce((html, post)=>{
     return html+`
     <div class='post'>
-      <div class='post__img'></div>
+      <div class='post__img' style="background-image:url('${images[i++].download_url}')"></div>
       <div >
         <h2>${post.title}</h2>
         <p class='post__description'>${post.body}</p>
@@ -19,4 +31,6 @@ fetchPosts().then(posts => {
     </div>`
   },'');
   mount.innerHTML = postsHTML
-});
+}
+
+renderPost();
